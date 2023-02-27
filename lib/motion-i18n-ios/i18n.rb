92c6e6750e5a599e3c7b38ea074c63308e316e3b
buffpojken <daniel@sykewarrior.com>
2023-02-27 16:45:17 +0100

    wtf

--- lib/motion-i18n-ios/i18n.rb
+++ lib/motion-i18n-ios/i18n.rb
@@ -3,6 +3,7 @@ module I18n
     def translate(key, substitutions = {})
       str = (NSBundle.mainBundle.localizedStringForKey(key, value:"", table:nil))
       puts str.inspect
+      puts str.methods.inspect
       String.new("laser").tap do |result|
         substitutions.each do |key, value|
           result.gsub!("%{#{key}}", value.to_s)
