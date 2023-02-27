a63a2093e2806453b2215b6f0e1447413e743621
buffpojken <daniel@sykewarrior.com>
2023-02-27 16:55:08 +0100

    tweaks

--- lib/motion-i18n-ios/i18n.rb
+++ lib/motion-i18n-ios/i18n.rb
@@ -6,7 +6,7 @@ module I18n
       if str.is_a?(ImmediateRef)
         puts str.methods.inspect
       end
-      String.new("laser").tap do |result|
+      String.new(str).tap do |result|
         substitutions.each do |key, value|
           result.gsub!("%{#{key}}", value.to_s)
         end
