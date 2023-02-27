f180ceb89a8eb1179dea87c26bcb27763926f49a
buffpojken <daniel@sykewarrior.com>
2023-02-27 16:41:29 +0100

    wtf

--- lib/motion-i18n-ios/i18n.rb
+++ lib/motion-i18n-ios/i18n.rb
@@ -1,11 +1,12 @@
 module I18n
   class << self
     def translate(key, substitutions = {})
-      String.new((NSBundle.mainBundle.localizedStringForKey(key, value:"", table:nil))).tap do |result|
-        substitutions.each do |key, value|
-          result.gsub!("%{#{key}}", value.to_s)
-        end
-      end
+      "mungo"
+      # String.new((NSBundle.mainBundle.localizedStringForKey(key, value:"", table:nil))).tap do |result|
+      #   substitutions.each do |key, value|
+      #     result.gsub!("%{#{key}}", value.to_s)
+      #   end
+      # end
     end
     alias t translate
 
