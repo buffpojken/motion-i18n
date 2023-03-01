d6304825dbf692ed542d955f6817c776d59e9add
buffpojken <daniel@sykewarrior.com>
2023-03-01 13:40:08 +0100

    fixing frozen string issue

--- lib/motion-i18n-ios/i18n.rb
+++ lib/motion-i18n-ios/i18n.rb
@@ -2,7 +2,7 @@ module I18n
   class << self
     def translate(key, substitutions = {})
       str = (NSBundle.mainBundle.localizedStringForKey(key, value:"", table:nil))
-      str.tap do |result|
+      str.dup.tap do |result|
         substitutions.each do |key, value|
           result.gsub!("%{#{key}}", value.to_s)
         end
